84a3d1cb3d7f8150431bbc7c7246508de2261b70
Waltor <longmengua@gmail.com>
2022-08-06 13:01:53 +0800

    update

--- pages/api/banxa-buy-nft.ts
+++ pages/api/banxa-buy-nft.ts
@@ -15,25 +15,25 @@ export default async function banxaBuyNft(
     res.status(401).send({ message: 'Bad request' })
     return
   }
-  const json: BanxaNFTOrderI = typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
+  const json: BanxaNFTOrderI<any> = typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
   // Unique customer reference provided by you to identify the order in our system
   const account_reference: string = uuid();
   console.log('account_reference', account_reference);
-  const data: BanxaNFTOrderI = {
+  const data: BanxaNFTOrderI<any> = {
     account_reference: account_reference,
     source: 'USD',
     source_amount: '100',
-    target: 'USDT',
+    target: 'SOL',
     target_amount: '1',
     wallet_address: '0xfc586533B5847309389818Bb8a00A8EA246B651C',
     return_url_on_success: 'http://localhost:3000/successful',
     meta_data: {
       nft: {
         contract_address: '0xfc586533B5847309389818Bb8a00A8EA246B651C',
-        token_id: '1',
+        token_id: account_reference,
       }
     }
   }
-  const outcome = await BANXA_PAYMENT.createNFT(data);
+  const outcome = await BANXA_PAYMENT.createNFTOrder(data, 'buy');
   res.status(200).json(outcome)
 }
